ce25ff19658b18ceb40b045ad2cfde0e352ba478
moviefan322 <philman202@yahoo.com>
2023-06-06 11:27:06 -0400

    navigates to filtered events programatically

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import {useRouter, NextRouter} from "next/router";
 import { getFeaturedEvents, Event } from "@/dummy-data";
 import EventList from "../components/events/EventList";
 import EventSearch from "@/components/events/EventSearch";
 
 function HomePage(): JSX.Element {
   const featuredEvents: Event[] = getFeaturedEvents();
+  const router: NextRouter = useRouter();
+
+  function findEventsHandler(year: string, month: string) {
+    const fullPath: string = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList events={featuredEvents} />
     </div>
   );
