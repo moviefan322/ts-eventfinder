650c9ec8281615ec0f44fa07f94dbd3d6e3278ac
moviefan322 <philman202@yahoo.com>
2023-06-07 13:27:26 -0400

    prerenders data for events/index.tsx

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { getAllEvents, Event } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { GetStaticProps } from "next";
+import { getAllEvents, Event } from "@/helpers/api-util";
 import EventList from "@/components/events/EventList";
+import EventSearch from "@/components/events/EventSearch";
 
-function EventsPage(): JSX.Element {
-  const allEvents: Event[] = getAllEvents();
+type Props = {
+  events: Event[];
+};
+
+function EventsPage(props: Props): JSX.Element {
+  const router = useRouter();
+  const allEvents: Event[] = props.events;
+
+  function findEventsHandler(year: string, month: string) {
+    const fullPath: string = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList events={allEvents} />
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const allEvents: Event[] = await getAllEvents();
+
+  return {
+    props: {
+      events: allEvents,
+    },
+    revalidate: 60,
+  };
+};
 
 export default EventsPage;
