765a0439caa9988d7fed4ab08e1302fb008e738a
moviefan322 <philman202@yahoo.com>
2023-06-07 13:56:26 -0400

    reconfigures slug page to use client-side data fetching

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter, NextRouter } from "next/router";
+import useSWR from "swr";
 import { getFilteredEvents, Event } from "@/helpers/api-util";
 import EventList from "@/components/events/EventList";
 import ResultsTitle from "@/components/events/results-title";
@@ -16,21 +17,57 @@ type Props = {
 };
 
 function FilteredEventsPage(props: Props): JSX.Element {
-  // const router: NextRouter = useRouter();
+  const [loadedEvents, setLoadedEvents] = useState<Event[]>([]);
+  const router: NextRouter = useRouter();
 
-  // const filteredData: string | string[] | undefined = router.query.slug;
+  const filteredData: string | string[] | undefined = router.query.slug;
 
-  // if (!filteredData) {
-  //   return <p className="center">Loading...</p>;
-  // }
+  const { data, error } = useSWR(
+    "https://nextjs-db-98d2d-default-rtdb.firebaseio.com/events.json"
+  );
 
-  // const filteredYear: string = filteredData[0];
-  // const filteredMonth: string = filteredData[1];
+  useEffect(() => {
+    if (data) {
+      const events: Event[] = [];
 
-  // const numYear: number = +filteredYear;
-  // const numMonth: number = +filteredMonth;
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
 
-  if (props.hasError) {
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear: string = filteredData[0];
+  const filteredMonth: string = filteredData[1];
+
+  const numYear: number = +filteredYear;
+  const numMonth: number = +filteredMonth;
+
+  const fileteredEvents = loadedEvents.filter((event: Event) => {
+    const eventDate: Date = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <>
         <ErrorAlert>
@@ -60,7 +97,7 @@ function FilteredEventsPage(props: Props): JSX.Element {
 
   return (
     <div>
-      <ResultsTitle date={new Date(props.date.year, props.date.month - 1)} />
+      <ResultsTitle date={new Date(numYear, numMonth - 1)} />
       <EventList events={filteredEvents} />
     </div>
   );
