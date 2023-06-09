cf40ccd3d2bcce071c2fad24a35885ddffeaf35a
moviefan322 <philman202@yahoo.com>
2023-06-09 15:19:03 -0400

    connects to mongoDB

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { email } = req.body;
 
@@ -8,6 +10,14 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+
+    const client = (await MongoClient.connect(
+      process.env.MONGO_URI
+    )) as MongoClient;
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: email });
+
+    client.close();
 
     res.status(201).json({ message: "Signed up!" });
   }
