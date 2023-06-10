67dc9c47cce42f92b584602c7c62de8ed076ecea
moviefan322 <philman202@yahoo.com>
2023-06-10 14:37:19 -0400

    brings context into notification component on layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./header";
+import Notification from "../ui/notification";
+import NotificationContext from "../../store/notification-context";
 
 type layoutProps = {
   children: React.ReactNode;
 };
 
 function Layout(props: layoutProps) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <Header />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
